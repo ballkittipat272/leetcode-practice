fc7f6f45939577c31690c7996c159a85b48fcdc2
kittipat <ballkittipat272@hotmail.com>
2025-03-01 23:41:02 +0700

    approach 1 for 205

--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+START: 20:35, END: , STOP: 21:20
+
+
+*/
+
+bool IsIsomorphic(string s, string t)
+{
+    if (s.Length != t.Length)
+        return false;
+
+    HashSet<char> hsS = new HashSet<char>();
+    HashSet<char> hsT = new HashSet<char>();
+    Dictionary<char, char> dict = [];
+    int idx = 0;
+
+    while (idx < s.Length)
+    {
+        char charS = s[idx];
+        char charT = t[idx];
+        hsS.Add(charS);
+        hsT.Add(charT);
+
+        if (dict.ContainsKey(charS))
+        {
+            if (dict[charS] != charT)
+                return false;
+        }
+        else
+        {
+            dict.Add(charS, charT);
+        }
+
+        idx++;
+    }
+
+    return hsS.Count == hsT.Count;
+}
+
+
+string s, t;
+bool yes;
+s = "badc";
+t = "baba";
+
+yes = IsIsomorphic(s, t);
+
+
+int end = 0;
