35497522963973e89fccc287cac7b3ecdf4852e2
kittipat <ballkittipat272@hotmail.com>
2025-03-01 21:26:42 +0700

    approach 1 for 125

--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+START: 20:35, END: , STOP: 21:20
+
+
+*/
+
+bool IsPalindrome(string s)
+{
+    if (s == null) return false;
+    s = s.ToLower().Trim();
+
+    int left, right;
+    left = 0;
+    right = s.Length - 1;
+
+    while (left < right)
+    {
+        if (!Char.IsLetterOrDigit(s[left]))
+        {
+            left++;
+            continue;
+        }
+        if (!Char.IsLetterOrDigit(s[right]))
+        {
+            right--;
+            continue;
+        }
+
+        if (s[left] != s[right])
+            return false;
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+
+string s;
+bool isPalindrome;
+
+s = "A man, a plan, a canal: Panama";
+isPalindrome = IsPalindrome(s);
+
+s = "race a car";
+isPalindrome = IsPalindrome(s);
+
+s = " ";
+isPalindrome = IsPalindrome(s);
+
+int end = 0;
