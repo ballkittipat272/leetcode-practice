de93b36984a00eb910c3cacd29e7622a2e99605f
kittipat <ballkittipat272@hotmail.com>
2025-03-01 22:28:45 +0700

    approach 1 for 392

--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+START: 20:35, END: , STOP: 21:20
+
+
+*/
+
+bool IsSubsequence(string s, string t)
+{
+    if (s == t) return true;
+
+    int j = 0;
+    for (int i = 0; i < s.Length; i++)
+    {
+        char charctr = s[i];
+
+        bool match = false;
+        while (j < t.Length)
+        {
+            char c = t[j++];
+            if (charctr == c)
+            {
+                match = true;
+                break;
+            }
+        }
+        if (match)
+            continue;
+
+        return false;
+    }
+
+    return true;
+}
+
+
+string s, t;
+bool yes;
+s = "abc";
+t = "ahbgdc";
+
+yes = IsSubsequence(s, t);
+
+
+int end = 0;
