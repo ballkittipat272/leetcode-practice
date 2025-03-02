b5ab3a691790f48b8dc3e4d29b06cd2c753004ee
kittipat <ballkittipat272@hotmail.com>
2025-03-02 11:50:05 +0700

    approach 1 for 228

--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+START: 11:09, END: 11:47
+
+*/
+
+IList<string> SummaryRanges(int[] nums)
+{
+    List<string> list = new List<string>();
+    if (nums.Length < 1)
+        return list;
+
+    int start = nums[0];
+    int end = nums[0];
+
+    for (int idx = 1; idx < nums.Length; idx++)
+    {
+        int num = nums[idx];
+        if (num != end + 1)
+        {
+            if (start != end)
+                list.Add($"{start}->{end}");
+            else
+                list.Add($"{start}");
+            start = num;
+        }
+        end = num;
+    }
+
+    if (start != end)
+        list.Add($"{start}->{end}");
+    else
+        list.Add($"{start}");
+
+    return list;
+}
+
+int[] nums;
+IList<string> output;
+nums = [0, 1, 2, 4, 5, 7];
+output = SummaryRanges(nums);
+
+nums = [0, 2, 3, 4, 6, 8, 9];
+output = SummaryRanges(nums);
+
+int end = 0;
