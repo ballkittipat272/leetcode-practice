080090071b264327d730931a38d8c590917ddb63
kittipat <ballkittipat272@hotmail.com>
2025-03-02 22:09:38 +0700

    approach 1 for 9

--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* START: 21:35, END:   */
+
+bool IsPalindrome(int x)
+{
+    string str = x.ToString();
+    int left, right;
+    left = 0;
+    right = str.Length - 1;
+    while (left <= right)
+    {
+        char chrLeft = str[left++];
+        char chrRight = str[right--];
+        if (chrLeft != chrRight)
+            return false;
+    }
+    return true;
+}
+
+int param1;
+bool output;
+
+//  11
+//   1
+// 100
+param1 = 121;
+output = IsPalindrome(param1);
+
+param1 = -121;
+output = IsPalindrome(param1);
+
+int end = 0;
