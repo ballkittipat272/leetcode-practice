98ac2eed035325a3970fb4ef381978035f8120c2
kittipat <ballkittipat272@hotmail.com>
2025-03-02 20:06:43 +0700

    approach 1 for 35

--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* START: 19:40, END:   */
+
+int SearchInsert(int[] nums, int target)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        int num = nums[i];
+        if (target <= num)
+            return i;
+    }
+
+    return nums.Length;
+}
+
+int[] param1;
+int param2;
+int output;
+
+param1 = [1, 3, 5, 6];
+param2 = 5;
+output = SearchInsert(param1, param2);
+
+param1 = [1, 3, 5, 6];
+param2 = 2;
+output = SearchInsert(param1, param2);
+
+param1 = [1, 3, 5, 6];
+param2 = 7;
+output = SearchInsert(param1, param2);
+
+int end = 0;
