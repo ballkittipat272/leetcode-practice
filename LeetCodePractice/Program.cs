0589a22c784a00ac5562bb819c593268ea9ecc47
kittipat <ballkittipat272@hotmail.com>
2025-03-02 22:58:32 +0700

    approach 1 for 70

--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* START: 21:35, END:   */
+
+int ClimbStairs(int n)
+{
+    int[] mem = new int[n + 1];
+    return Climb(n, mem);
+}
+
+int Climb(int n, int[] mem)
+{
+    if (n <= 2)
+        return n;
+    if (mem[n] != 0)
+        return mem[n];
+    mem[n] = Climb(n - 1, mem) + Climb(n - 2, mem);
+    return mem[n];
+}
+
+int param1;
+int output;
+
+param1 = 2;
+output = ClimbStairs(param1);
+
+param1 = 3;
+output = ClimbStairs(param1);
+
+int end = 0;
