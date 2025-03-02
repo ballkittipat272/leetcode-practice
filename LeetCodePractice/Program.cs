a8ab1b26162bad1a33b66b16b560c55e5b63e1db
kittipat <ballkittipat272@hotmail.com>
2025-03-02 11:05:23 +0700

    approach 1 for 290

--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+START: , END: , STOP: 
+
+
+*/
+
+bool WordPattern(string pattern, string s)
+{
+    string[] splittedWords = s.Split(' ');
+
+    if (pattern.Length != splittedWords.Length)
+    {
+        return false;
+    }
+
+    Dictionary<char, string> dict = new Dictionary<char, string>();
+
+    int idx = 0;
+    while (idx < pattern.Length)
+    {
+        char chr = pattern[idx];
+        string word = splittedWords[idx];
+
+        if (dict.TryGetValue(chr, out string? value))
+        {
+            if (value != word)
+                return false;
+        }
+        else
+        {
+            if (dict.ContainsValue(word))
+            {
+                return false;
+            }
+            dict.Add(chr, word);
+        }
+
+        idx++;
+    }
+    return true;
+}
+
+string pattern, s;
+bool ans;
+pattern = "abba";
+s = "dog cat cat fish";
+
+ans = WordPattern(pattern, s);
+
+
+int end = 0;
