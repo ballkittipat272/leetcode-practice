db083e1acf4573d963655f1432fcc84ad0271901
kittipat <ballkittipat272@hotmail.com>
2025-03-04 00:28:03 +0700

    approach 2 for 80

--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* START: 23:39, END: 00:11
+** RE-TRY: , END TRY: 
+** RE-TRY 2: , END TRY 2: 
+
+num = 1; i = 0; 
+num = 1; i = 0; 
+num = 1; i = 1; 
+num = 2; i = 2; 
+
+*/
+
+int RemoveDuplicates(int[] nums)
+{
+    int j = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        int num = nums[i];
+        if (j < 2 || num != nums[j - 2])
+        {
+            nums[j] = num;
+            j++;
+        }
+    }
+    return j;
+}
+
+int[] param1;
+int param2;
+int[] param3;
+int param4;
+int output;
+
+param1 = [1, 1, 1, 2, 2, 3];
+output = RemoveDuplicates(param1);
+
+param1 = [0, 0, 1, 1, 1, 1, 2, 3, 3];
+output = RemoveDuplicates(param1);
+
+int end = 0;
