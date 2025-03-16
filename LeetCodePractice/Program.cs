9ff189c78470e962283a07c6707e7f11cdfec85c
kittipat <ballkittipat272@hotmail.com>
2025-03-16 23:58:44 +0700

    approach 1 for 3-from-kkps

--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* START: 23:09, END: 23:49
+** RE-TRY: , END TRY: 
+** RE-TRY 2: , END TRY 2: 
+*/
+
+string LengthOfLongestSubstring(string s)
+{
+    if (s.Length < 2)
+        return s;
+    int left = 0;
+    int right = 1;
+    string visited = "" + s[0];
+    int maxLength = 0;
+    string maxWord = "";
+    while (right < s.Length)
+    {
+        char chr = s[right];
+        while (visited.Contains(chr))
+        {
+            visited = visited.Substring(1);
+            left++;
+        }
+        visited += chr;
+        if (right - left + 1 > maxLength)
+        {
+            maxLength = Math.Max(maxLength, right - left + 1);
+            maxWord = s.Substring(left, maxLength);
+        }
+        right++;
+    }
+    return maxWord;
+}
+
+string param1;
+string output;
+
+param1 = "xxxxaxabcdexxabcxx"; // "xabcde"
+output = LengthOfLongestSubstring(param1);
+
+param1 = "xxxxaxabcdexyz1234xabcxx"; // "abcdexyz1234"
+output = LengthOfLongestSubstring(param1);
+
+int end = 0;
