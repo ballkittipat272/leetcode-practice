926bb59b6d07c53aa4ffe176d3418b52fb3c4036
kittipat <ballkittipat272@hotmail.com>
2025-03-19 21:42:32 +0700

    approach 1 for 122

--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* START: , END: 
+** RE-TRY: , END TRY: 
+** RE-TRY 2: , END TRY 2: 
+*/
+
+int MaxProfit(int[] prices)
+{
+    if (prices.Length < 2)
+        return 0;
+    int buy = 0, sell = 1;
+    int profit = 0;
+    while (sell < prices.Length)
+    {
+        int buyPrice = prices[buy];
+        int sellPrice = prices[sell];
+        if (buyPrice < sellPrice)
+        {
+            profit += sellPrice - buyPrice;
+        }
+        buy = sell;
+        sell++;
+    }
+    return profit;
+}
+
+int[] param1;
+int output;
+
+param1 = [7, 1, 5, 3, 6, 4];
+output = MaxProfit(param1); // 7
+
+param1 = [1, 2, 3, 4, 5];
+output = MaxProfit(param1); // 4
+
+param1 = [7, 6, 4, 3, 1];
+output = MaxProfit(param1); // 0
+
+int end = 0;
