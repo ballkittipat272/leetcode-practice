644b37cfb277a678ad6118dcb54391a179a0d06e
kittipat <ballkittipat272@hotmail.com>
2025-03-19 16:06:06 +0700

    approach 1 for 209

--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* START: , END: 
+** RE-TRY: , END TRY: 
+** RE-TRY 2: , END TRY 2: 
+*/
+
+int MinSubArrayLen(int target, int[] nums)
+{
+    int left = 0;
+    int sum = 0;
+    int length = 0;
+    int minLength = 0;
+    for (int i = left; i < nums.Length && left < nums.Length; i++)
+    {
+        sum += nums[i];
+        length++;
+        if (sum >= target)
+        {
+            if (minLength == 0 || length < minLength)
+                minLength = length;
+            sum = 0;
+            length = 0;
+            left++;
+            i = left - 1;
+        }
+    }
+    return minLength;
+}
+
+int param1;
+int[] param2;
+int output;
+
+param1 = 7;
+param2 = [2, 3, 1, 2, 4, 3];
+output = MinSubArrayLen(param1, param2);
+
+param1 = 4;
+param2 = [1, 4, 4];
+output = MinSubArrayLen(param1, param2);
+
+param1 = 11;
+param2 = [1, 1, 1, 1, 1, 1, 1, 1];
+output = MinSubArrayLen(param1, param2);
+
+int end = 0;
